4c694b7c50da1ee9673755eff4c1b9f70a5674b8
Tom Mezaros <tom@mezaros.com>
2014-01-19 01:07:03 -0500

    Icon
    Added icon, may or not be scaled properly, will see when converted to
    .exe

--- Snapshot/Properties/Resources.Designer.cs
+++ Snapshot/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Snapshot.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap snap {
             get {
                 object obj = ResourceManager.GetObject("snap", resourceCulture);
